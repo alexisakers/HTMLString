356533e264e711654d5f11e0d35176f3f2149909
Alexis Aubry Radanovic <aleks@alexis-aubry-radanovic.me>
2017-02-13 22:27:43 +0100

    Remove benchmark CLI

--- Package.swift
+++ Package.swift
@@ -1,17 +1,5 @@
 import PackageDescription
 
-let targets = [
-    Target(name: "HTMLString"),
-    // Target(name: "Performance", dependencies: [.Target(name: "HTMLString")])
-]
-
-let excludes: [String] = [
-    "Sources/Performance",
-    "HTMLStringObjC"
-]
-
 let package = Package(
-    name: "HTMLString",
-    targets: targets,
-    exclude: excludes
+    name: "HTMLString"
 )
