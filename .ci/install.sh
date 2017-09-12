3240803163293b770e6cf9ac517e26fd4a9a5162
Alexis Aubry <me@alexaubry.fr>
2017-09-12 10:42:31 +0200

    Upgrade dependencies

--- .ci/install.sh
+++ .ci/install.sh
@@ -8,7 +8,8 @@ fi
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get update
-    sudo apt-get install clang libicu-dev libstdc++6-4.7-dev -y
+    sudo apt-get install clang libicu-dev libstdc++6-4.7-dev -f -y
+    sudo apt-get upgrade
     strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX
     echo "4.0-DEVELOPMENT-SNAPSHOT-2017-08-31-a" >> .swift-version
 fi
