a8e1bbb66e896a6e7701e7690d05d60cfdfb045f
Alexis Aubry <me@alexaubry.fr>
2017-09-12 12:51:07 +0200

    Do not use test toolchain

--- .ci/install.sh
+++ .ci/install.sh
@@ -6,10 +6,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 fi
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get update
-    sudo apt-get install clang libicu-dev libstdc++6-4.7-dev -f -y
-    sudo apt-get upgrade
+    sudo apt-get install clang libicu-dev libstdc++6-4.6-dev -f -y
     strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX
     echo "4.0-DEVELOPMENT-SNAPSHOT-2017-08-31-a" >> .swift-version
 fi
