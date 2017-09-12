a62a200d732d9a7e70f7827b149a7063688f31c8
Alexis Aubry <me@alexaubry.fr>
2017-09-12 09:56:30 +0200

    Fix Linux system dependencies

--- .ci/install.sh
+++ .ci/install.sh
@@ -6,5 +6,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 fi
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    sudo apt-get install libstdc++6
     echo "4.0-DEVELOPMENT-SNAPSHOT-2017-08-31-a" >> .swift-version
+    eval "$(curl -sL https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/9f442512a46d7a2af7b850d65a7e9bd31edfb09b/swiftenv-install.sh)"
+    export SWIFT_VERSION="4.0-DEVELOPMENT-SNAPSHOT-2017-08-31-a"
 fi
