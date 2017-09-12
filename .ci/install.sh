be0e90ddbb74be7c35549e5376f74773593033bc
Alexis Aubry <me@alexaubry.fr>
2017-09-12 10:35:48 +0200

    Force APT install

--- .ci/install.sh
+++ .ci/install.sh
@@ -6,9 +6,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 fi
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get update
-    sudo apt-get install clang libicu-dev libstdc++6-4.7-dev
+    sudo apt-get install clang libicu-dev libstdc++6-4.7-dev -y
     strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX
     echo "4.0-DEVELOPMENT-SNAPSHOT-2017-08-31-a" >> .swift-version
 fi
